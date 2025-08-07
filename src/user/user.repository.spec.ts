b1ee44b58523442cdcb98bf200917fbcbadfb57f
Mardianto <dwi.da174@gmail.com>
2025-08-08 02:09:03 +0800

    update

--- src/user/user.repository.spec.ts
+++ src/user/user.repository.spec.ts
@@ -1,41 +1,18 @@
-import { UserRepository } from './user.repository';
 import { UserDTO } from './entities/user.entities';
 
-describe('UserRepository', () => {
-  let repo: UserRepository;
+export class UserRepository {
+  private users: UserDTO[] = [];
 
-  beforeEach(() => {
-    repo = new UserRepository();
-  });
+  create(user: UserDTO): UserDTO {
+    this.users.push(user);
+    return user;
+  }
 
-  it('should create a user', () => {
-    const user: UserDTO = {
-      id: 1,
-      email: 'test@example.com',
-      name: 'Test User',
-      phone: '081234567890',
-      number_ktp: '1234567890123456',
-      password: 'password',
-      role_user: 'ADMIN',
-    };
-    expect(repo.create(user)).toEqual(user);
-  });
+  findAll(): UserDTO[] {
+    return this.users;
+  }
 
-  it('should find all users', () => {
-    expect(repo.findAll()).toBeInstanceOf(Array);
-  });
-
-  it('should find one user by id', () => {
-    const user: UserDTO = {
-      id: 2,
-      email: 'test2@example.com',
-      name: 'Test User 2',
-      phone: '081234567891234',
-      number_ktp: '1234567890123457',
-      password: 'password2',
-      role_user: 'CUSTOMER',
-    };
-    repo.create(user);
-    expect(repo.findOne(2)).toEqual(user);
-  });
-});
+  findOne(id: number): UserDTO | undefined {
+    return this.users.find(user => user.id === id);
+  }
+}
